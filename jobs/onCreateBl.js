550c60691f6cc4a05507624a39e19ea30dbc824b
wikoci <74468439+wikoci@users.noreply.github.com>
2021-10-27 16:00:39 +0200

    Update onCreateBl.js

--- jobs/onCreateBl.js
+++ jobs/onCreateBl.js
@@ -65,7 +65,9 @@ async function onCreateBl() {
 
           db.syncOnCreate.insert({ last_time: moment().format("YYYY-MM-DD") });
         })
-        .catch((err) => {});
+        .catch((err) => {
+          console.log(err);
+        });
 
       request.on("done", (result) => {
         // Always emitted as the last one
