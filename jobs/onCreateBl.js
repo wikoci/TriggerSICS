fa8d7160b0c8c70302e97afd3149661a528fa42b
wikoci <74468439+wikoci@users.noreply.github.com>
2021-11-19 11:51:22 +0100

    push

--- jobs/onCreateBl.js
+++ jobs/onCreateBl.js
@@ -49,7 +49,13 @@ async function onCreateBl() {
  select CT_Num as code_client,do_piece as code_bl,ar_ref as code_article,dl_qte,dl_ligne,dl_poidsnet,
  dl_montantht,dl_montantttc,do_Date,cbmodification 
   from F_DOCLIGNE 
-  where DO_Date > '${latest_date}' and do_type in (1,3)`); // or request.execute(procedure)
+  where DO_Date > '${latest_date}' 
+  and do_type in (1,3) 
+  and f_comptet.co_no >= 42 
+and f_comptet.co_no not in ('46','47')
+and CT_Intitule like 'VDR%'
+  
+  `); // or request.execute(procedure)
 
       console.log(response);
 
